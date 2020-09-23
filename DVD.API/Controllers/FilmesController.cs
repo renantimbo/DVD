e97fcd86f7d1a84b0d949c9ade88972de8728acb
Renan Timbó Silva <renan.timbo@hbsis.com.br>
2020-09-23 00:50:25 -0300

    Iniciando API

---
+++ DVD.API/Controllers/FilmesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DVD.Context.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DVD.API.Controllers
+{
+    [ApiController]
+    [Route("Filmes")]
+    public class FilmesController : ControllerBase
+    {
+        public FilmesController()
+        {
+        }
+
+        [HttpGet]
+        [Route("FilmesGet")]
+        public ActionResult GetFilmes()
+        {
+            var result = new Filme()
+            {
+                Nome = "De volta para o futuro I"
+            };
+            return Ok(result);
+        }
+    }
+}
+
