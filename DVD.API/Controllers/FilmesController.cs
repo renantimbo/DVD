455753254d533d96f37af0eacf751caa6ca5c206
Renan Timbó Silva <renan.timbo@hbsis.com.br>
2020-09-23 11:48:11 -0300

    Testes de Get, Post, Busca e Delete iniciais da API

--- DVD.API/Controllers/FilmesController.cs
+++ DVD.API/Controllers/FilmesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DVD.Context.Models;
+using DVD.Util;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,7 +18,7 @@ namespace DVD.API.Controllers
         }
 
         [HttpGet]
-        [Route("FilmesGet")]
+        [Route("GetFilmes")]
         public ActionResult GetFilmes()
         {
             var result = new Filme()
@@ -25,6 +26,41 @@ namespace DVD.API.Controllers
                 Nome = "De volta para o futuro I"
             };
             return Ok(result);
+        }
+
+        public static List<Filme> listaFilme = new List<Filme>();
+
+        [HttpPost]
+        [Route("PostFilmes")]
+        public ActionResult PostFilmes(Filme Filme)
+        {
+            listaFilme.Add(Filme);
+
+            return Ok(listaFilme);
+        }
+
+        [HttpGet]
+        [Route("BuscaFilmes")]
+        public ActionResult GetFilmes(string Filme)
+        {
+            var result = listaFilme.Where(q => q.Nome.Contains(Filme)).ToList();
+
+            if (result.Count == 0)
+                return BadRequest(Message.NoSuccess);
+            else
+                return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("DeleteFilmes")]
+        public ActionResult DeleteFilmes(string Filme)
+        {
+            var result = listaFilme.RemoveAll(q => q.Nome == Filme);
+
+            if (result == 0)
+                return BadRequest(Message.NoSuccess);
+            else
+                return Ok(Message.Success);
         }
     }
 }
---
+++ DVD.Util/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DVD.Util
+{
+    public static class Message
+    {
+        public static string Success => "Deu bom";
+        public static string NoSuccess => "Deu ruim";
+    }
+}
